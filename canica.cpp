2468cb180eb5301412887dc68723eeb0bcebdd21
giuxpp <giuspianfort@gmail.com>
2023-10-08 00:34:05 -0600

    Ahora las canicas se pueden apilar

--- canica.cpp
+++ canica.cpp
@@ -32,11 +32,13 @@
 /***** MACROS Section *****/
 #define window_width       2000  
 #define window_height      2000
-#define FPS                30  //original=60  // This is the number of frames per second to render.
+#define FPS                50  //original=60  // This is the number of frames per second to render.
 #define Y_COORDENATE_INIT  0.95
 #define X_COORDENATE_INIT  0.5
 #define SIZE_OF_ARRAY      100
-
+#define TOTAL_Y_POSITIONS  30
+#define BIT_MAP_SIZE       16
+#define BIT_MAP_IMAGE      canica16_bitmap //canica_bitmap  canica16_bitmap
 
 enum {
     RGB_R =  0,
@@ -64,20 +66,24 @@ GLubyte canica_bitmap[]={
 0x00,    0x0F,    0xF0,    0x00, 0x00,    0x00,    0x00,    0x00,
 };
 
+
+
 GLubyte canica16_bitmap[]={
 0b00000000, 0b00000000,
 0b00000111, 0b11100000,
 0b00011111, 0b11111000,
+0b00011111, 0b11111000,
 0b00111111, 0b11111100,
 0b00111111, 0b11111100,
-0b01111111, 0b11111110,
-0b01111111, 0b11111110,
+0b00111111, 0b11111100,
 0b01111111, 0b11111110,
 0b01111111, 0b11111110,
 0b01111111, 0b11111110,
 0b01111111, 0b11111110,
 0b00111111, 0b11111100,
 0b00111111, 0b11111100,
+0b00111111, 0b11111100,
+0b00011111, 0b11111000,
 0b00011111, 0b11111000,
 0b00000111, 0b11100000,
 0b00000000, 0b00000000,  
@@ -118,7 +124,7 @@ class canica{
         bool isDone;
 
     public:
-        int coor[2];
+        int matrixCoor[2];
         int barIndex;
         static int bars[20];        
 
@@ -128,25 +134,25 @@ class canica{
             color[RGB_B] = randomFloat();
             x_coor = X_COORDENATE_INIT;
             y_coor = Y_COORDENATE_INIT;
-            coor[0] = 10;
-            coor[1] = 20;
+            matrixCoor[0] = 10;
+            matrixCoor[1] = TOTAL_Y_POSITIONS;
             isDone = false;
             barIndex = 10;
         }
 
-        double getCoorX(){
+        double getDisplayPositionX(){
             return x_coor;
         }
 
-        double getCoorY(){
+        double getDisplayPositionY(){
             return y_coor;
         }
 
-        void setCoorX(double c){
+        void setDisplayPositionX(double c){
             if (c<255) x_coor=c;
         }
 
-        void setCoorY(double c){
+        void setDisplayPositionY(double c){
             if (c<255) y_coor=c;
         }
 
@@ -172,67 +178,71 @@ class canica{
 
 canica canicas[SIZE_OF_ARRAY];
 
-int canMatrix[20][20]={0};
+int canMatrix[20][200]={0};
+int canicaVector[20]={0};
 
 /***** STANDARD Namespace *****/
 using namespace std;
 
 /* LOCAL VARIABLES  Section */
-//static double x=0, y=4;  // Coordinates of the Canica
-static int x_pos=0;
+
 
 
 /***** LOCAL FUNCTIONS Section *****/
 void printXcordenates();
+void printBitmap(GLubyte * ptr, int size);
 
 void drawCanica(canica * canicaPtr){
     glColor3f(canicaPtr->getRGB(RGB_R), canicaPtr->getRGB(RGB_G), canicaPtr->getRGB(RGB_B));    
-    glRasterPos3f(canicaPtr->getCoorX(), canicaPtr->getCoorY(), 0.0); //(X, Y, ?, ? )
-    glBitmap(32, 32, 0, 0, 0, 0, canica_bitmap);   
+    glRasterPos3f(canicaPtr->getDisplayPositionX(), canicaPtr->getDisplayPositionY(), 0.0); //(X, Y, ?, ? )
+    glBitmap(BIT_MAP_SIZE, BIT_MAP_SIZE, 0, 0, 0, 0, BIT_MAP_IMAGE);   
 }
 
-#define Y_STEP             0.05
-#define X_STEP             0.025          
+#define Y_STEP             0.03
+#define X_STEP             0.015          
 
 void moveCanica(canica * canicaPtr){
-  double dispPosition_y = canicaPtr->getCoorY();
-  double dispPosition_x = canicaPtr->getCoorX();  
+  double dispPosition_y = canicaPtr->getDisplayPositionY();
+  double dispPosition_x = canicaPtr->getDisplayPositionX();  
   static int tId = 0;
 
   /* Validate that the ball is still in the screen by Y position */
-  if ((dispPosition_y>0) && (canicaPtr->getIsDone()==false) ){
-    dispPosition_y-= Y_STEP;      
-    canicaPtr->coor[COORDENATE_Y] -= 1;
-    
-    /* If ball is in brownian range, make the x movement */
-    if (dispPosition_y>0.35){        
-        cout<<tId++<<endl;
-        if (rand()%2) {
-            dispPosition_x += X_STEP;
-            x_pos++;        
-            canicaPtr->coor[COORDENATE_X] += 1;
-        } 
-        else {
-            dispPosition_x -= X_STEP;
-            x_pos--;
-            canicaPtr->coor[COORDENATE_X] -= 1;
+  dispPosition_y-= Y_STEP;  
+  if ((canicaPtr->matrixCoor[COORDENATE_Y]>canicaVector[canicaPtr->matrixCoor[COORDENATE_X]]) /* && (canicaPtr->getIsDone()==false)*/ /*&& (canicaVector[canicaPtr->matrixCoor[COORDENATE_Y]]  )*/  ){
+         
+    if (true){
+        canicaPtr->matrixCoor[COORDENATE_Y] -= 1;
+        // cout<<tId++<<endl;
+        /* If ball is in brownian range, make the x movement */
+        if (dispPosition_y>0.35){        
+            //cout<<canicaPtr->matrixCoor[COORDENATE_X]<<endl;
+            if (rand()%2) {
+                dispPosition_x += X_STEP;      
+                canicaPtr->matrixCoor[COORDENATE_X] += 1;
+            } 
+            else {
+                dispPosition_x -= X_STEP;
+                canicaPtr->matrixCoor[COORDENATE_X] -= 1;
+            }
         }
     } else {
         /* Actions to take when ball finished brownian movement */
         tId=0;
+        cout<<endl;
 
     }
 
 
-    canicaPtr->setCoorX(dispPosition_x);
-    canicaPtr->setCoorY(dispPosition_y);
+    canicaPtr->setDisplayPositionX(dispPosition_x);
+    canicaPtr->setDisplayPositionY(dispPosition_y);
   }
   else  { // here the Ball gas reach the bottom.
     canicaPtr->setIsDone(true);    
-    canMatrix[20-canicaPtr->coor[COORDENATE_Y]][canicaPtr->coor[COORDENATE_X]] = 1;
+    canMatrix[20-canicaPtr->matrixCoor[COORDENATE_Y]][canicaPtr->matrixCoor[COORDENATE_X]] = 1;
+    canicaVector[canicaPtr->matrixCoor[COORDENATE_X]]++;
   }
 
-    //printXcordenates();
+    printXcordenates();
 }
 
 
@@ -269,8 +279,15 @@ void display() {
     }    
     glFlush(); // Giux: se necesita?????
 }
+bool myflag=true;
 
 int main(int argc, char** argv) {
+   
+    if (myflag) {
+        //printBitmap(BIT_MAP_IMAGE, BIT_MAP_SIZE);
+        myflag=false;
+    }
+    
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     //glutInitWindowPosition(80, 80);
@@ -290,6 +307,20 @@ int main(int argc, char** argv) {
 
 void printXcordenates(){
     for (int i=0; i<=SIZE_OF_ARRAY; i++){
-        cout<<"canica "<<i <<"coord X= "<<canicas[i].getCoorX() <<endl;       
+       // cout<<"canica "<<i <<"coord X= "<<canicas[i].getDisplayPositionX() <<endl;       
+    }
+}
+
+void printBitmap(GLubyte * ptr, int size)
+{
+    for (int i=0; i<size; i)
+    {
+        for (int j=0; j<size/2; j++)
+        {
+            char c = (ptr[i+j]&0x80)?'X':' ';
+            ptr[i+j]>>=1;
+            cout<<c;
+        }
+        cout<<endl;
     }
 }
